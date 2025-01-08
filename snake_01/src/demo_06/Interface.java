be40abc6a8fc29f4d43ac8989e1de7fee3fcaac1
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-08 11:17:27 +0900

    Test_01_01_job

---
+++ snake_01/src/demo_06/Interface.java
@@ -0,0 +1,5 @@
+package demo_06;
+
+public interface Interface {
+	public void execute();
+}
---
+++ snake_01/src/snake_01/Test_01_01_job.java
@@ -0,0 +1,18 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Test_01_01_job
+{ 
+  public static void main( String[] args )
+  { 
+    Calc01 calc = new Calc01( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc01.create();
+        save.execute();
+    });
+  }
+}
+
