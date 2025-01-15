8419ba98476644ba792eb7e870981bf8f55945c0
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-15 12:40:04 +0900

    Test_01_06_home

---
+++ snake_01/src/snake_01/Test_01_06_home.java
@@ -0,0 +1,19 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Test_01_06_home
+{ 
+  public static void main( String[] args )
+  { 
+    Calc06 calc = new Calc06( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc06.create();
+        save.execute();
+
+    });
+  }
+}
+
