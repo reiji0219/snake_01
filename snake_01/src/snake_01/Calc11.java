400ed2cf53caee7ae45cffa8d09b1f80fae7ba7e
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-16 23:36:21 +0900

    Test_01_11_home

---
+++ snake_01/src/snake_01/Calc11.java
@@ -0,0 +1,47 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Calc11 extends Item11
+{
+  private String name;
+
+  public Calc11( String name ){
+    super();
+    this.name = name;
+  }
+
+  @Override
+public void display(){
+    indi( name );
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc11 calc = new Calc11( "Poly-morphism" );
+
+      Item11.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc ); 
+
+        System.out.println( "計算式 :" + item.getId());
+
+        item.display();
+        
+        item.execute();
+      });
+    };
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
+
---
+++ snake_01/src/snake_01/Test_01_11_home.java
@@ -0,0 +1,17 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Test_01_11_home
+{
+  public static void main( String[] args )
+  {
+    Calc11 calc = new Calc11( "Poly-morphism" );
+    calc.process( () -> {
+
+    Interface save = Calc11.create();
+      save.execute();
+    });
+  }
+}
+
