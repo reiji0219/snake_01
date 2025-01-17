f81645977cf41b5b6c63ecc3a3bb92cd93b5084a
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-17 22:49:53 +0900

    Test_01_17_job

---
+++ snake_01/src/snake_01/Test_01_17_job.java
@@ -0,0 +1,20 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Test_01_17_job
+{ 
+  public static void main( String[] args )
+  { 
+    Calc17 calc = new Calc17( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc17.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
+}
+
