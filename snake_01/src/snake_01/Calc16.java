5ae6a41ee7156941b96878960fa9855feb66c230
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-21 15:33:56 +0900

    Test_01_16_home

---
+++ snake_01/src/snake_01/Calc16.java
@@ -0,0 +1,45 @@
+package snake_01;
+
+import demo_06.Interface;
+
+public class Calc16 extends Item16
+{
+  private String name;
+
+  public Calc16( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+     Calc16 calc = new Calc16( "Poly-morphism" );
+
+     Item16.save( item -> {
+       item.id( "掛け算" )
+             .name( "税込み価格" )
+             .price( 980 )
+             .tax( 1.1 )
+             .calc( calc );
+
+       System.out.println( "計算式 :" + item.getId() );
+
+       item.display();
+      });
+    };
+  }
+
+  @Override
+public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
+
