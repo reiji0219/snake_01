bc89714790703f0aedbdf06e252405d38d58862f
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-22 18:10:21 +0900

    Test_01_21_home

--- snake_01/snake_01/under_01/Calc21.java
+++ snake_01/snake_01/under_01/Calc21.java
@@ -1,5 +1,46 @@
 package under_01;
 
-public class Calc21 {
+import demo_06.Interface;
 
+public class Calc21 extends Item21
+{
+	private String name;
+
+	public Calc21( String name ) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc21 calc = new Calc21( "Poly-morphism" );
+
+			Item21.save(item -> {
+				item.id( "掛け算" )
+					  .name( "税込み価格" )
+					  .price( 980 )
+					  .tax( 1.1 )
+					  .calc( calc );
+
+				System.out.println( "計算式 :" + item.getId() );
+
+				item.display();
+
+				item.execute();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi(name);
+	}
+
+	public void process( Interface inter ) {
+		indi( "start" );
+
+		inter.execute();
+
+		indi( "end" );
+	}
 }
--- snake_01/snake_01/under_01/Test_01_21_home.java
+++ snake_01/snake_01/under_01/Test_01_21_home.java
@@ -1,5 +1,19 @@
 package under_01;
 
-public class Test_01_21_home {
+import demo_06.Interface;
 
+public class Test_01_21_home
+{
+	public static void main( String[] args ) 
+	{
+		Calc21 calc = new Calc21( "Poly-morphism" );
+		calc.process(() -> {
+			System.out.println( "関心事を記述" );
+
+			Interface save = Calc21.create();
+			save.execute();
+
+			System.out.println( "Hello World_1" );
+		});
+	}
 }
