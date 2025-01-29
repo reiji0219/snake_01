db7df1a1c2e7a6ab0e0e043cb7e3bfdaec970534
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-29 14:58:18 +0900

    Test_01_26_home

---
+++ snake_01/snake_01/under_01/Calc26.java
@@ -0,0 +1,44 @@
+package under_01;
+
+import demo_06.Interface;
+
+public class Calc26 extends Item26
+{
+	private String name;
+
+	public Calc26( String name ) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc26 calc = new Calc26( "Poly-morphism" );
+
+			Item26.save(item -> {
+				item.id( "掛け算" )
+					  .name( "税込み価格" )
+					  .price( 980 )
+					  .tax( 1.1 )
+					  .calc( calc );
+
+				System.out.println( "計算式 :" + item.getId() );
+
+				item.display();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi( name );
+	}
+
+	public void process( Interface inter ) {
+		indi( "start" );
+
+		inter.execute();
+
+		indi( "end" );
+	}
+}
