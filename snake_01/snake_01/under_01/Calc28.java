802cac7d7e82269f80989e87d766fd0fce90d0e6
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-01-31 06:23:56 +0900

    Test_01_28_home

---
+++ snake_01/snake_01/under_01/Calc28.java
@@ -0,0 +1,45 @@
+package under_01;
+
+import demo_06.Interface;
+
+public class Calc28 extends Item28
+{ 
+  private String name;
+
+  public Calc28( String name ){ 
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){ 
+    return() -> { 
+      Calc28 calc = new Calc28( "Poly-morphism" );
+
+      Item28.save( item -> { 
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+      });
+    };
+  }
+
+  @Override
+public void display(){ 
+    indi( name );
+  }
+
+  public void process( Interface inter ){ 
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
+
---
+++ snake_01/snake_01/under_01/Test_01_28_home.java
@@ -0,0 +1,20 @@
+package under_01;
+
+import demo_06.Interface;
+
+public class Test_01_28_home
+{
+  public static void main( String[] args )
+  {
+    Calc28 calc = new Calc28( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc28.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
+}
+
