b9202202da0a7628e071b20da7e50024e9d76057
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-02-12 23:45:32 +0900

    add Leap Stream_API

---
+++ snake_01/src/leapYear/LeapYear.java
@@ -0,0 +1,38 @@
+package leapYear;
+
+/**
+ * うるう年
+ * @author reiji
+ * 
+ */
+public class LeapYear 
+{
+
+	/**
+	 * うるう年の判定ロジック
+	 * 400で割り切れる : うるう年
+	 * 上記以外で100で割り切れる : うるう年ではない
+	 * 上記以外で4で割り切れる : うるう年
+	 * 上記以外 : うるう年ではない
+	 *  @param year 西暦の年
+	 * @return
+	 */
+
+	public boolean isLeapYear( int year ) {
+		boolean result = false;
+		
+		if (year <= 0 ) {
+			result = false;
+		}else if (year % 400 == 0 ) {
+			result = true;
+		}else if ( year % 100 == 0 ) {
+			result = false;
+		}else if ( year % 4 == 0 ) {
+			result = true;
+		}else {
+			result = false;
+		}
+		
+		return result;
+	}
+}
---
+++ snake_01/src/stream_API/Stream_API.java
@@ -0,0 +1,56 @@
+package stream_API;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream_API 
+{
+	//こんな「 Member」レコードがあって、
+
+	public record Member ( String name, int age )
+	{
+
+	//こんなList<Member>があるとします
+
+	List<Member> memberList = List.of();
+		new Member( "佐々木", 27 ).
+		new Member( "中野", 20 ).
+		new Member( "田中", 19 ).
+		new Member( "小田", 16 );
+
+	
+
+
+
+	//18歳以上のメンバーの名前だけのリストを作成するコード
+	// for,if が有る（ベタな記述）
+
+	List<String> resultList = new ArrayList<>();
+
+	for( Member member : memberList ){
+		if( member.age() >= 18 ){
+			String name = member.name();
+			resultList.add( name );
+		}
+	}
+
+	// Stream APIは、Listなどのコレクションに対して
+	// 抽出や変換を行う API です。Stream APIを使用すると
+	// ①何がやりたいかすぐに理解出来る。
+	// ②for,if が不要なのでバグを生みにくい
+
+
+
+	// List を Stream に変換する
+
+	List<String> resultList2 = memberList.stream()
+		// 18歳以上のメンバーのみ抽出する
+		.filter( member -> member.age() >= 18 )
+		// Member から String（メンバーの名前）に変換する
+		.map( member -> member.name() )
+		// List に変換する
+		.toList( ); 
+						
+	}
+	}
+}
