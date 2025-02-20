ac6293df3c1a1ead565a1bca77b54198097a529a
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-02-20 21:43:52 +0900

    Test_02_19_home

---
+++ snake_01/snake_02/middle_02/Calc19.java
@@ -0,0 +1,47 @@
+package middle_02;
+
+import demo_06.Interface;
+
+public class Calc19 extends Item19
+{
+  private String name;
+
+  public Calc19( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc19 calc = new Calc19( "Poly-morphism" );
+
+      Item19.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+
+        item.execute();
+      });
+    };
+  }
+
+  @Override
+public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
+
