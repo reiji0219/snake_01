a8e91eb98be98be14b2c7d7e100527e3c23f8f10
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-02-25 20:04:08 +0900

    Test_02_25_job

---
+++ snake_01/snake_02/under_02/Item25_1.java
@@ -0,0 +1,78 @@
+package under_02;
+
+import demo_06.Interface;
+import java.util.function.Consumer;
+
+public class Item25_1 implements Interface
+{
+  @Override
+public void execute(){
+    indi( "Lambda" );
+
+  }
+
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+  private Calc25_1 calc;
+
+
+  public Item25_1 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item25_1 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item25_1 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item25_1 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public Item25_1 calc( Calc25_1 calc ){
+    this.calc = calc;
+    return this;
+  }
+
+  public static void save( Consumer<Item25_1> con ){
+    Item25_1 item = new Item25_1();
+    con.accept( item );
+    indi( "save :" + item );
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public void display(){
+    indi( "表示内容 :" + name );
+
+    double calcInTax = calcTax( price, tax );
+      indi( "税込み価格は" + calcInTax + "円です" );
+
+    calc.display();
+  }
+
+  public static double calcTax( double price, double tax ){
+    return( price * tax );
+  }
+
+  @Override
+public String toString(){
+    return( "Item25_1" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax );
+  }
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
+}
+
