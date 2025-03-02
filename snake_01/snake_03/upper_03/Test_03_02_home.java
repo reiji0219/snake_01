43e6c885063667f4b21b1400c83e36a201d37945
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-03-02 20:29:54 +0900

    Test_03_02_home

---
+++ snake_01/snake_03/upper_03/Test_03_02_home.java
@@ -0,0 +1,20 @@
+package upper_03;
+
+import demo_06.Interface;
+
+public class Test_03_02_home
+{ 
+  public static void main( String[] args )
+  { 
+    Calc02 calc = new Calc02( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc02.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
+}
+
