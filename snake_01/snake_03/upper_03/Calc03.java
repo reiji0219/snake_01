60ede92f870bdda50386659362da1ad80ea9399c
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-03-03 20:20:38 +0900

    Test_03_03_job

---
+++ snake_01/snake_03/upper_03/Calc03.java
@@ -0,0 +1,47 @@
+package upper_03;
+
+import demo_06.Interface;
+
+public class Calc03 extends Item03
+{
+  private String name;
+
+  public Calc03( String name ){ 
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){ 
+    return() -> { 
+      Calc03 calc = new Calc03( "Poly-morphism" );
+
+      Item03.save( item -> { 
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+        
+        item.execute();
+      });
+    };
+  }
+
+  @Override
+public void display(){ 
+    indi( name );
+  }
+
+  public void process( Interface inter ){ 
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+  }
+}
+
---
+++ snake_01/snake_03/upper_03/Test_03_03_job.java
@@ -0,0 +1,20 @@
+package upper_03;
+
+import demo_06.Interface;
+
+public class Test_03_03_job
+{
+  public static void main( String[] args )
+  {
+    Calc03 calc = new Calc03( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc03.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
+}
+
