8de4da9f1e2a086bd2a823d4ece1e7e832d21392
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-03-12 08:38:18 +0900

    Test_03_10_job

--- snake_01/snake_03/upper_03/Test_03_10_job.java
+++ snake_01/snake_03/upper_03/Test_03_10_job.java
@@ -1,5 +1,20 @@
 package upper_03;
 
-public class Test_03_10_job {
+import demo_06.Interface;
 
+public class Test_03_10_job
+{
+  public static void main( String[] args )
+  {
+    Calc10 calc = new Calc10( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc10.create();
+        save.execute();
+
+      System.out.println( "ArrayList.add" );
+    });
+  }
 }
+
