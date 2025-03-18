81d8a7b2e8c3c26cb1f0348f640e1342dbfc71db
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-03-18 21:38:44 +0900

    Test_03_12_home

--- snake_01/src/main/java/snake/snake_03/middle_03/Test_03_12_home.java
+++ snake_01/src/main/java/snake/snake_03/middle_03/Test_03_12_home.java
@@ -1,7 +1,17 @@
 package main.java.snake.snake_03.middle_03;
 
-public class Test_03_12_home 
-{
+import demo_06.Interface;
 
-	
+public class Test_03_12_home {
+	public static void main(String[] args) {
+		Calc12 calc = new Calc12("Poly-morphism");
+		calc.process(() -> {
+			System.out.println("関心事を記述");
+
+			Interface save = Calc12.create();
+			save.execute();
+
+			System.out.println("Hello World_1");
+		});
+	}
 }
