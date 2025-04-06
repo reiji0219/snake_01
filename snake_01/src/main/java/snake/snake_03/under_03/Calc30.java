9471348640bd931168d8c923014fef77cb1df38f
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-04-06 20:53:54 +0900

    Test_03_30_home

---
+++ snake_01/src/main/java/snake/snake_03/under_03/Calc30.java
@@ -0,0 +1,47 @@
+package main.java.snake.snake_03.under_03;
+
+import demo_06.Interface;
+
+public class Calc30 extends Item30
+{
+  private String name;
+
+  public Calc30( String name ){
+    super();
+    this.name = name;
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc30 calc = new Calc30( "Poly-morphism" );
+
+      Item30.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+
+        item.execute();
+      });
+    };
+  }
+
+  @Override
+public void display(){
+    indi( name );
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+   }
+}
+
---
+++ snake_01/src/main/java/snake/snake_03/under_03/Test_03_30_home.java
@@ -0,0 +1,21 @@
+package main.java.snake.snake_03.under_03;
+
+import demo_06.Interface;
+
+public class Test_03_30_home
+{
+  public static void main( String[] args )
+  {
+    Calc30 calc = new Calc30( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc30.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
+}
+
+
