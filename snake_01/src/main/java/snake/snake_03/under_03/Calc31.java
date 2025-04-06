966f7d4c2a945f873f255cf3c085265429c38cc4
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-04-06 21:01:43 +0900

    Test_03_31_home

---
+++ snake_01/src/main/java/snake/snake_03/under_03/Calc31.java
@@ -0,0 +1,45 @@
+package main.java.snake.snake_03.under_03;
+
+import demo_06.Interface;
+
+public class Calc31 extends Item31
+{
+  private String name;
+
+  public Calc31( String name ){
+    super();
+    this.name = name;
+  }
+
+  @Override
+public void display(){
+    indi( name );
+  }
+
+  public static Interface create(){
+    return() -> {
+      Calc31 calc = new Calc31( "Poly-morphism" );
+
+      Item31.save( item -> {
+        item.id( "掛け算" )
+            .name( "税込み価格" )
+            .price( 980 )
+            .tax( 1.1 )
+            .calc( calc );
+
+        System.out.println( "計算式 :" + item.getId() );
+
+        item.display();
+      });
+    };
+  }
+
+  public void process( Interface inter ){
+    indi( "start" );
+
+    inter.execute();
+
+    indi( "end" );
+   }
+}
+
---
+++ snake_01/src/main/java/snake/snake_03/under_03/Item31.java
@@ -0,0 +1,77 @@
+package main.java.snake.snake_03.under_03;
+
+import java.util.function.Consumer;
+import demo_06.Interface;
+
+public class Item31 implements Interface
+{
+  @Override
+public void execute(){
+    indi( "Lambda" );
+
+  }
+
+  private String id;
+  private String name;
+  private double price;
+  private double tax;
+  private Calc31 calc;
+
+
+  public Item31 id( String id ){
+    this.id = id;
+    return this;
+  }
+
+  public Item31 name( String name ){
+    this.name = name;
+    return this;
+  }
+
+  public Item31 price( double price ){
+    this.price = price;
+    return this;
+  }
+
+  public Item31 tax( double tax ){
+    this.tax = tax;
+    return this;
+  }
+
+  public Item31 calc( Calc31 calc ){
+    this.calc = calc;
+    return this;
+  }
+
+  public static void save( Consumer<Item31> con ){
+    Item31 item = new Item31();
+    con.accept( item );
+    indi( "save :" + item );
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public void display(){
+    indi( "表示内容 :" + name );
+
+    double calcInTax = calcTax( price, tax );
+      indi( "税込み価格は" + calcInTax + "円です" );
+    calc.display();
+  }
+
+  public static double calcTax( double price, double tax ){
+    return( price * tax );
+  }
+
+  @Override
+public String toString(){
+    return( "Item31 :" + "id=" + id + " " +  "name=" + name + " " +  "price=" + price + " " +  "tax=" + tax );
+  }
+
+  public static void indi( String s0 ){
+    System.out.println( s0 );
+  }
+}
+
