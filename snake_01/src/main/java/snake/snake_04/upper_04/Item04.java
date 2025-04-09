fc5d885e4a9d7c9729e5f018c27046d649349afc
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-04-09 17:07:17 +0900

    Test_04_03_home

---
+++ snake_01/src/main/java/snake/snake_04/upper_04/Item04.java
@@ -0,0 +1,5 @@
+package main.java.snake.snake_04.upper_04;
+
+public class Item04 {
+
+}
---
+++ snake_01/src/main/java/snake/snake_04/upper_04/Test_04_03_home.java
@@ -0,0 +1,11 @@
+package main.java.snake.snake_04.upper_04;
+
+public class Test_04_03_home 
+{
+	public static void main( String[] args )
+	{
+		
+		
+		System.out.println( "Hello World" );
+	}
+}
