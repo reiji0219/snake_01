ce42d64cbdcefd99c4dfc14776b06908fca05a4c
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2025-04-09 17:15:54 +0900

    Test_04_03_home

--- snake_01/src/main/java/snake/snake_04/upper_04/Calc04.java
+++
@@ -1,5 +0,0 @@
-package main.java.snake.snake_04.upper_04;
-
-public class Calc04 {
-
-}
--- snake_01/src/main/java/snake/snake_04/upper_04/Test_04_03_home.java
+++ snake_01/src/main/java/snake/snake_04/upper_04/Test_04_03_home.java
@@ -1,11 +1,20 @@
 package main.java.snake.snake_04.upper_04;
 
-public class Test_04_03_home 
+import demo_06.Interface;
+
+public class Test_04_03_home
 {
-	public static void main( String[] args )
-	{
-		
-		
-		System.out.println( "Hello World" );
-	}
+  public static void main( String[] args )
+  {
+    Calc03 calc = new Calc03( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc03.create();
+        save.execute();
+
+      System.out.println( "Hello World_1" );
+    });
+  }
 }
+
